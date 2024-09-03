1434f3a9781d25c8eda71ac841d3941b3fcb537f
Dylan van Dijk <dylanvdijk1@gmail.com>
2024-09-03 16:16:40 -0400

    Initial Commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MainPageComponent } from './main-page/main-page.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MainPageComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [
+    provideClientHydration()
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
